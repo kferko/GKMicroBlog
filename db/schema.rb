470eeb891b191c273f1eab2e336515e685632962
Gregory Park <gregoryjpark@gmail.com>
2015-12-28 16:09:50 -0500

    add create_reviews_table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228210311) do
+ActiveRecord::Schema.define(version: 20151228210646) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string  "description"
+    t.integer "user_id"
+    t.integer "dish_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
