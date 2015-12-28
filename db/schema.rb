0e42194140444e47e9512c297c33785ed930266b
Gregory Park <gregoryjpark@gmail.com>
2015-12-28 18:00:45 -0500

    add migration to create user friendships table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228214259) do
+ActiveRecord::Schema.define(version: 20151228224410) do
 
   create_table "dishes", force: :cascade do |t|
     t.string  "name"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20151228214259) do
     t.string  "description"
     t.integer "user_id"
     t.integer "dish_id"
+  end
+
+  create_table "user_friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "user_friend_id"
   end
 
   create_table "users", force: :cascade do |t|
--- models.rb
+++ models.rb
@@ -1,22 +1,29 @@
 class User < ActiveRecord::Base
-  has_many :reviews
+	has_many :reviews
+	has_many :user_friends, through: :user_friendships
+	has_many :user_friendships
 end
 
 class Review < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :dish
+	belongs_to :user
+	belongs_to :dish
 end
 
 class Owner < ActiveRecord::Base
-  has_one :menu
+	has_one :menu
 end
 
 class Menu < ActiveRecord::Base
-  belongs_to :owner
-  has_many :dishes
+	belongs_to :owner
+	has_many :dishes
 end
 
 class Dish < ActiveRecord::Base
-  belongs_to :menu
-  has_many :reviews
+	belongs_to :menu
+	has_many :reviews
+end
+
+class UserFriendship < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :user_friend, class_name: "User"
 end
