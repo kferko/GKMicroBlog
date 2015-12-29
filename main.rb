422e91b8e5ad9a4c1426be4f5898e5c0b34a0783
Gregory Park <gregoryjpark@gmail.com>
2015-12-28 20:26:11 -0500

    add form and route to create new user, add placeholder success view

--- main.rb
+++ main.rb
@@ -12,5 +12,31 @@ set :sessions, :true
 
 get '/' do
 #  "Welcome to DishDish!"
-  erb :login
+	erb :login
+end
+
+post '/create-user' do
+	if !Owner.where(username: params[:username]).first.nil?
+		puts "There is already an Owner with that name!" #TODO: Add flash message
+		erb :login
+	elsif !User.where(username: params[:username]).first.nil?
+		puts "There is already a User with that name!" #TODO: Add flash message
+		erb :login
+	elsif params[:password] != params[:password_confirmation]
+		puts "Those passwords don't match!" #TODO: Add flash message
+		erb :login
+	### TODO: Add validation of username (restrict to alphanumeric, length, etc.)
+	### TODO: Add validation of password (restrict to alphanumeric, length, etc.)
+	else 
+		User.create(username: params[:username], password: params[:password])
+		puts "User #{params[:username]} created!!!"
+		session[:user_id] = User.where(username: params[:username]).first.id
+		erb :new_user_success
+	end
+end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
 end
