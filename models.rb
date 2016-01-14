3a4139708f8e4f19b59836daf45b7a601b1c29d0
Greg Park <gregoryjpark@gmail.com>
2016-01-14 13:42:07 -0500

    fix change-password route, add comments

--- models.rb
+++ models.rb
@@ -14,14 +14,17 @@ class User < ActiveRecord::Base
 		self.user_friends
 	end
 
+	# check whether a user is followed by another id
 	def followed_by?(user_id)
 		followers.any?{|follower| follower.id == user_id}
 	end
 
+	# check whether a user is following another id
 	def follows?(user_id)
 		friends.any?{|friend| friend.id == user_id}
 	end
 
+	# return array of all (or a quantity of) reviews by a users friends
 	def recent_friend_reviews(quantity=nil)
 		friend_reviews = self.friends.map { |friend| friend.reviews }
 		friend_reviews.flatten!
@@ -32,6 +35,7 @@ class User < ActiveRecord::Base
 		end
 	end
 
+	# return array of all (or a quantity of) recent reviews by a user
 	def recent_reviews(quantity=nil)
 		if quantity
 			self.reviews.sort_by{ |review| review.id }.reverse[0..quantity-1]
@@ -40,26 +44,34 @@ class User < ActiveRecord::Base
 		end
 	end
 
+	# return most recently written review 
 	def most_recent_review
 		self.reviews.sort_by{ |review| review.id }.reverse.first
 	end
 
+	# return a concatenation of user's first and last name
 	def public_name
 		self.firstname.capitalize + " " + self.lastname[0].upcase + "."
 	end
 
+	# return a concatenation of a user's city and state
 	def public_location
 		self.city.capitalize + ", " + self.state.upcase
 	end
 
+	# return a review with a score equal to the highest score given
+	# by this user (will ignore other tying reviews)
 	def highest_review 
 		self.reviews.sort_by{ |review| review.rating }.reverse.first
 	end
 
+	# return a review with a score equal to the lowest score given
+	# by this user (will ignore other tying reviews)
 	def lowest_review 
 		self.reviews.sort_by{ |review| review.rating }.first
 	end
 
+	# calculate average rating across all ratings given by this user
 	def average_rating
 		all_ratings = self.reviews.map{|review| review.rating}
 		average_rating = all_ratings.reduce(0){|sum, rating| sum + rating} / all_ratings.count.to_f
@@ -73,6 +85,7 @@ class Review < ActiveRecord::Base
 	belongs_to :dish
 	belongs_to :menu
 
+	# return several (4 by default) recently written reviews (by anyone)
 	def self.most_recent(quantity=4)
 		self.all.sort_by{ |review| review.id }.reverse[0..quantity-1]
 	end
@@ -82,6 +95,8 @@ class Owner < ActiveRecord::Base
 # each owner (aka restaurant) creates one menu
 	has_one :menu
 
+	# return whether this owner has a menu
+	# not used in final version
 	def has_menu?
 		!self.menu.nil?
 	end
@@ -94,16 +109,19 @@ class Menu < ActiveRecord::Base
 	has_many :dishes
 	has_many :reviews
 
+	# return highest (ignoring other ties) scoring review for this menu
 	def highest_review 
 		self.reviews.sort_by{|review| review.rating }.reverse.first
 	end
 
+	# return average of all ratings of dishes on this menu
 	def average_rating
 		all_ratings = self.reviews.map{|review| review.rating}
 		average_rating = all_ratings.reduce(0){|sum, rating| sum + rating} / all_ratings.count.to_f
 		'%.0f' % average_rating # rounds to nearest integer for now
 	end
 
+	# return quantity (or all) recently reviews of dishes from this menu
 	def recent_reviews(quantity=nil)
 		if quantity
 			self.reviews.sort_by{ |review| review.id }.reverse[0..quantity-1]
@@ -118,14 +136,17 @@ class Dish < ActiveRecord::Base
 	belongs_to :menu
 	has_many :reviews
 
+	# returns whether the dish has any reviews
 	def has_reviews?
 		!self.reviews.empty? 
 	end
 
+	# return array of all ratings for this dish
 	def ratings
 		self.reviews.map{|review| review.rating}
 	end
 
+	# return average of all ratings for this dish
 	def average_rating 
 		if has_reviews?
 			average_rating = ratings.reduce(0){|sum, rating| sum + rating} / ratings.count.to_f
@@ -135,6 +156,7 @@ class Dish < ActiveRecord::Base
 		end
 	end
 
+	# returns whether this dish was ever reviewed by a given user
 	def ever_reviewed_by?(user_id)
 		self.reviews.any?{|review| review.user_id == user_id}
 	end
